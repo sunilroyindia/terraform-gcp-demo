10e21aea3a7c90b375fcd278bd0565260690af99
Sunil Roy <34080332+sunilroyindia@users.noreply.github.com>
2025-09-15 21:28:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "no-public-access" {
   name          = "sunil-project-1986-bucket"
   location      = "US"
   force_destroy = true
-  project        = sunil-project-1986
+  project        = Default Project
   public_access_prevention = "enforced"
 }
