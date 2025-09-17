a9c349a5a9c356cf77d3539924f676927c1c0ecd
Sunil Roy <sunilroy.india@gmail.com>
2025-09-17 06:13:44 +0530

    changes in main tf

--- main.tf
+++ main.tf
@@ -1,13 +1,32 @@
-resource "google_storage_bucket" "no-public-access" {
-  name          = "sunil-project-1986-bucket"
-  location      = "US"
-  force_destroy = true
-  project        = "Default Project"
-  public_access_prevention = "enforced"
+# main.tf
+
+provider "google" {
+  credentials = var.GOOGLE_CRED
+  project     = var.project
+  region      = var.region
 }
 
 variable "GOOGLE_CRED" {
-      type        = string
-      sensitive   = true # Mark as sensitive if it contains sensitive data
-      description = "GCP Authentication"
-    }
+  description = "GCP service account credentials in JSON"
+  type        = string
+  sensitive   = true
+}
+
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "US"
+}
+
+resource "google_storage_bucket" "no-public-access" {
+  name          = "sunil-project-1986-bucket"
+  location      = var.region
+  force_destroy = true
+  project       = var.project
+  public_access_prevention = "enforced"
+}
